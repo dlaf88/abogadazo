bf2c55a71e9875987e51ac82a885e7ea1d336a0d
Diego Lafuente <dlaf88@gmail.com>
2015-04-14 14:09:44 +0000

    added answer create to questions and showed the answers on each question page

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,6 +3,15 @@ class AnswersController < ApplicationController
     @answer = Answer.new
   end 
   def create
+    @question = Question.find(params[:question_id])
+    @answer = @question.answers.build(params.require(:answer).permit(:description))
+    if @answer.save
+      flash[:notice] = "Answer was saved successfully."
+      redirect_to @question
+    else
+      flash[:error] = "Error creating answer. Please try again."
+      
+    end
     
   end 
 end
