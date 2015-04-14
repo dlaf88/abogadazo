544deed8206e377c13950b26f73f7c04d841629e
Diego Lafuente <dlaf88@gmail.com>
2015-04-14 11:37:08 +0000

    added question form and partial

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,35 @@
 class QuestionsController < ApplicationController
-  def index 
+  def index
+    @questions=Questions.all
+  end 
   
-  end
-  
-  def new
+  def new 
     @question = Question.new
   end 
   
-  def create
-    if @question.save
-      
-    end
+  def show
+    @question = Question.find(params[:id])
+  end 
+  
+  def edit 
+    @question = Question.find(params[:id])
   end
+  
+  def create
+    @question = Question.new(params.require(:question).permit(:title, :description, :phone))
+    if @question.save
+      redirect_to @question, notice: "Question was saved successfully."
+    else
+      flash[:error] = "Error creating question. Please try again."
+       render :new
+    end
+      
+  end 
+  
+  def update
+  end 
+  
+  
   
   
   
