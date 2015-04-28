1b5e64efb94041a56606425e7f50fcb4c5d4bc11
Diego Lafuente <dlaf88@gmail.com>
2015-04-28 14:45:43 +0000

    added answers to active admin

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,10 +1,12 @@
 class AnswersController < ApplicationController
+  before_filter :authenticate_lawyer!
   def new 
     @answer = Answer.new
   end 
   def create
     @question = Question.find(params[:question_id])
     @answer = @question.answers.build(params.require(:answer).permit(:description))
+    @answer.lawyer = current_lawyer
     if @answer.save
       flash[:notice] = "Answer was saved successfully."
       redirect_to @question
