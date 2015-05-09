a706c94acb085dc22d051346cb18299dde5556ab
Diego Lafuente <dlaf88@gmail.com>
2015-05-09 03:56:44 +0000

    added css index

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,7 @@
 class Question < ActiveRecord::Base
-  has_many :answers
+  has_many :answers 
+  validates_presence_of :description, {message: "Por favor es necesario una descripción de la pregunta."}
+  validates_presence_of :phone, {message: "Por favor necesitamos un telefono o email para poder informarte que tu pregunta ha sido contestada.
+"}
   
 end
