1fff15aca9dc4c3cb9b224f76df8891fe468d802
Diego <dlaf88@gmail.com>
2015-07-10 21:38:43 +0000

    testing the change on the welcome page

--- db/schema.rb
+++ db/schema.rb
@@ -88,7 +88,6 @@ ActiveRecord::Schema.define(version: 20150629031602) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.boolean  "verified"
     t.boolean  "validated",              default: false
   end
 
